fb12ac2a04d28d5bb945fc76a026a81493f2962a
Riyanto24434 <aris.riyanto24434@gmail.com>
2025-07-26 17:49:53 +0000

    Merapihkan Login dan Register

--- REGISTER (30-06-2025)(DONE)/script.js
+++ REGISTER (30-06-2025)(DONE)/script.js
@@ -1,16 +1,13 @@
-// Function to toggle password visibility for a given input field and its toggle icon
 function setupPasswordToggle(passwordFieldId, toggleIconSelector) {
   const passwordField = document.getElementById(passwordFieldId);
   const togglePassword = document.querySelector(toggleIconSelector);
 
   if (passwordField && togglePassword) {
     togglePassword.addEventListener("click", function () {
-      // Toggle the type attribute
       const type =
         passwordField.getAttribute("type") === "password" ? "text" : "password";
       passwordField.setAttribute("type", type);
 
-      // Toggle the eye icon
       this.querySelector("i").classList.toggle("fa-eye");
       this.querySelector("i").classList.toggle("fa-eye-slash");
     });
